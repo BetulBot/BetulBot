b59a0083eed45604fb1745f35067a17a47e3882f
Sorp <botsorp@gmail.com>
2020-01-26 13:28:38 +0100

    Added emoji remove function

--- src/commands/emoji.js
+++ src/commands/emoji.js
@@ -61,6 +61,22 @@ class EmojiCommand extends Command {
         }
     }
 
+    executeRemoveEmoji(msg, emoji) {
+        if (this.checkServer(msg)) {
+
+            msg.guild.deleteEmoji(emoji.id, "Vote").then(() => {
+
+                //Post success message with removed emoji
+                msg.channel.send("Removed emoji: ``:" + emoji.name + ":``");
+
+            }).catch((error) => {
+                msg.channel.send("Failed to remove emoji ``:" + emoji.name + "``.");
+                console.error(error);
+            });
+
+        }
+    }
+
     /*
         Vote functionality for the add command
     */
@@ -85,6 +101,35 @@ class EmojiCommand extends Command {
 
                     //No result, don't add emoji
                     msg.channel.send("Vote result for adding emoji ``:" + emojiName + ":``: 👎");
+
+                }
+
+            });
+
+        });
+    }
+
+    startRemoveEmojiVote(msg, options, emojiName, url, emoji) {
+        msg.channel.send(this.mention(msg.author) + " started a vote to remove following emoji:\n``:" + emojiName + ":``", options).then((message) => {
+            //Start vote on message sent by the bot
+            var vote = new Vote(message, this.TIME);
+
+            vote.execute().then((result) => {
+
+                if (result === true) {
+
+                    //Voted for yes, add emoji
+                    this.executeRemoveEmoji(msg, emoji);
+
+                } else if (result === false) {
+
+                    //Voted for no, don't remove emoji
+                    msg.channel.send("Vote result for removing emoji ``:" + emojiName + ":``: 👎");
+
+                } else if (result === null) {
+
+                    //No result, don't remove emoji
+                    msg.channel.send("Vote result for removing emoji ``:" + emojiName + ":``: 👎");
 
                 }
 
@@ -138,7 +183,7 @@ class EmojiCommand extends Command {
             var parsedEmoji = DUtil.parseEmoji(emoji);
 
             //Check if parsed emoji exists
-            if (parsedEmoji.id !== null) {
+            if (parsedEmoji !== null && parsedEmoji.id !== null) {
 
                 //Check if emoji is from this server
                 if (!msg.guild.emojis.has(parsedEmoji.id)) {
@@ -168,6 +213,39 @@ class EmojiCommand extends Command {
 
     }
 
+    removeEmoji(msg, args) {
+
+        var emoji = args[2];
+        var parsedEmoji = DUtil.parseEmoji(emoji);
+
+        //Check if parsed emoji exists
+        if (parsedEmoji !== null && parsedEmoji.id !== null) {
+
+            //Check if emoji is from this server
+            if (msg.guild.emojis.has(parsedEmoji.id)) {
+
+                var fileName = parsedEmoji.id + (parsedEmoji.animated ? ".gif" : ".png");
+                var url = "https://cdn.discordapp.com/emojis/" + fileName + "?v=1";
+
+                this.startRemoveEmojiVote(msg, {
+                    files: [{
+                        attachment: url,
+                        name: fileName
+                    }]
+                }, parsedEmoji.name, url, parsedEmoji);
+
+            } else {
+                msg.reply("You can't remove emojis from other servers.");
+            }
+
+        } else {
+
+            msg.reply("Invalid emoji.");
+
+        }
+
+    }
+
     execute(msg) {
 
         //Check if this command is executed on a server
@@ -188,7 +266,7 @@ class EmojiCommand extends Command {
 
                     } else if (command === "remove") {
 
-                        msg.reply("**FUNCTION WORK IN PROGRESS**");
+                        this.removeEmoji(msg, args);
 
                     } else {
 
